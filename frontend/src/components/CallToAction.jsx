2aab82ed7617fba2c02837502d81734e4311a90e
ChetanSharma76 <159418211+ChetanSharma76@users.noreply.github.com>
2025-08-11 01:34:08 +0530

    Update CallToAction.jsx

--- frontend/src/components/CallToAction.jsx
+++ frontend/src/components/CallToAction.jsx
@@ -19,7 +19,7 @@ const CallToAction = () => {
         whileTap={{ scale: 0.95 }}
         className="px-8 py-3 rounded-full bg-transparent border-2 border-purple-400 text-white font-semibold text-lg hover:bg-purple-500/20 transition-all duration-300 relative overflow-hidden group  hover:border-white animate-borderMove"
         >
-        <span} className="relative z-10">Create Account</span>
+        <span className="relative z-10">Create Account</span>
         <span className="absolute inset-0 bg-gradient-to-r from-purple-400/20 to-pink-300/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300  border-transparent hover:border-white animate-borderMove"></span>
         </motion.button>
     </div>
